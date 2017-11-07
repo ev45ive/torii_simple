66f47296e6a809f08b74afeb3391dea375150803
Mateusz Kulesza <m.kulesza@pjwstk.edu.pl>
2017-11-07 20:04:44 +0100

    13.7. Update Component

--- src/app/project-edit.component.ts
+++ src/app/project-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-project-edit',
@@ -7,15 +8,35 @@ import { ActivatedRoute } from "@angular/router";
     <p>
       project-edit works!
     </p>
+    <div>
+      <label>Name</label>
+      <input [(ngModel)]="project.name">
+      <button (click)="update()">Update</button>
+    </div>
+    <hr>
+    <button routerLink="/">Wroc do listy</button>
   `,
   styles: []
 })
 export class ProjectEditComponent implements OnInit {
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private http: HttpClient) { }
+
+  project = {}
+  
+  update(){    
+    this.http.put('http://localhost:3000/projects/'+ this.project['id'], this.project)
+    .subscribe( project => {
+      this.project = project
+    })
+  }
 
   ngOnInit() {
-    console.log(this.route.snapshot.paramMap.get('id'))
+    this.project['id'] = this.route.snapshot.paramMap.get('id')
+    this.http.get('http://localhost:3000/projects/'+ this.project['id'])
+    .subscribe( project => {
+      this.project = project
+    })
   }
 
 }
